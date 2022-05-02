e69aff032a89bb15c8f1867f133b5954403e2068
Anastasia <lireien@gmail.com>
2022-05-02 22:58:20 +0300

    feat: add binary-search-tree solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,127 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this._rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (this._rootNode == null) {
+      this._rootNode = new Node(data);
+    } else {
+      this.addNode(this._rootNode, data);
+    }
+  }
+  addNode(node, data) {
+    if (data < node.data) {
+      if (node.left == null) {
+        node.left = new Node(data);
+        return;
+      } else {
+        return this.addNode(node.left, data);
+      }
+    } else if (data > node.data) {
+      if (node.right == null) {
+        node.right = new Node(data);
+        return;
+      } else {
+        return this.addNode(node.right, data);
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let pointer = this._rootNode;
+    while (pointer) {
+      if (data == pointer.data) {
+        return true;
+      }
+      if (data < pointer.data) {
+        pointer = pointer.left;
+      } else {
+        pointer = pointer.right;
+      }
+    }
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let pointer = this._rootNode;
+    while (pointer.data !== data) {
+      if (data < pointer.data) {
+        pointer = pointer.left;
+      } else {
+        pointer = pointer.right;
+      }
+      if (pointer === null) {
+        return null;
+      }
+    }
+    return pointer;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this._rootNode = removeNode(this._rootNode, data);
+
+    function removeNode (node, data) {
+      if (node == null) {
+        return null;
+      }
+      if (data == node.data) {
+        if (node.left == null && node.right == null) {
+          return null;
+        }
+        if (node.left == null) {
+          return node.right;
+        }
+        if (node.right == null) {
+          return node.left;
+        }
+        let tmpNode = node.right;
+        while (tmpNode.left !== null) {
+          tmpNode = tmpNode.left;
+        }
+        node.data = tmpNode.data;
+        node.right = removeNode(node.right, tmpNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this._treeRoot = removeNode(this._treeRoot, data);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let pointer = this._rootNode;
+    while (pointer.left !== null) {
+      pointer = pointer.left;
+    }
+    return pointer.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let pointer = this._rootNode;
+    while (pointer.right !== null) {
+      pointer = pointer.right;
+    }
+    return pointer.data;
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
